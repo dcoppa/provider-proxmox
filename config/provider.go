6ea54001946d2f4d594e306487c0abf5e3f20fae
David Coppa <dcoppa@cocus.com>
2023-06-28 16:47:51 +0200

    Init

--- config/provider.go
+++ config/provider.go
@@ -10,12 +10,12 @@ import (
 
 	ujconfig "github.com/upbound/upjet/pkg/config"
 
-	"github.com/upbound/upjet-provider-template/config/null"
+	"github.com/dcoppa/provider-proxmox/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "proxmox"
+	modulePath     = "github.com/dcoppa/provider-proxmox"
 )
 
 //go:embed schema.json
